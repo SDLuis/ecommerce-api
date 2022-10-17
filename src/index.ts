0d4086330c66bb991d31a7f5217eb48c416d03ea
Luis <Luistejeda0011@gmail.com>
2022-10-17 17:46:34 -0300

    DB connection done

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import app from './libs/middlewares'
+import db from './models/db.model'
 
 /* eslint-disable */
 const PORT = process.env.PORT || 5000
@@ -6,4 +7,12 @@ const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
   console.log(`App is running at http://localhost:${PORT} in development mode
 Press CTRL-C to stop`)
+db.sequelize
+.authenticate()
+.then(async () => {
+  console.log("database connected");
+})
+.catch((e: any) => {
+  console.log(e.message);
+});
 })
