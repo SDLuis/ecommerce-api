3a2a7fe5feb2510223267f9aa93140be8cbb3584
Luis <Luistejeda0011@gmail.com>
2022-10-18 22:44:58 -0300

    Adding cookie parser

--- src/libs/middlewares.ts
+++ src/libs/middlewares.ts
@@ -3,10 +3,12 @@ import cors from 'cors'
 import authRoutes from '../routes/auth.route'
 import userRoutes from '../routes/user.route'
 import productRoutes from '../routes/products.route'
+import cookieparser from 'cookie-parser'
 
 const app = express()
 
 app.use(express.json())
+app.use(cookieparser())
 app.use(
   cors({
     credentials: true,
